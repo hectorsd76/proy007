c8e0fda6d7c0f8685202a9ff1ab32d3accaae19e
hsolana <hsolana@formacion.cic.es>
2025-07-16 16:25:48 +0200

    Inicio el proyecto

---
+++ src/test/java/es/cic/curso2025/proy007/Proy007ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso2025.proy007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proy007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
